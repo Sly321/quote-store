f75a354f13e9ed3f39d4fda6e4201078648d348c
Sven Liebig <liebigsv@gmail.com>
2018-10-14 20:20:26 +0200

    create a graphql server that can return quotes

--- client/index.js
+++ client/index.js
@@ -1,17 +1,24 @@
 const {
 	prisma
 } = require('../database/generated/prisma-client')
+const {
+	typeDefs
+} = require('../database/generated/prisma-client/prisma-schema')
 
-const express = require("express")
-const app = express()
+const {
+	GraphQLServer
+} = require("graphql-yoga")
+const {
+	Prisma
+} = require('prisma-binding')
 
 // A `main` function so that we can use async/await
 async function main() {
 
 	// Create a new user called `Alice`
 	const newQuote = await prisma.createQuote({
-		from: "That's what I do, I drink and I know things.",
-		text: "Tyrion Lannister"
+		text: "That's what I do, I drink and I know things.",
+		from: "Tyrion Lannister"
 	})
 
 	// Read all users from the database and print them to the console
@@ -19,4 +26,34 @@ async function main() {
 	console.log(allQuotes)
 }
 
-main().catch(e => console.error(e))
+// main().catch(e => console.error(e))
+
+
+const resolvers = {
+	Query: {
+		quotes: (root, args, ctx, info) => ctx.prisma.query.quotes({}, info),
+	},
+	Mutation: {
+		createQuote: (root, args, ctx, info) =>
+			ctx.prisma.mutation.createQuote({
+				data: {
+					text: args.text,
+					from: args.from
+				}
+			}, info),
+	},
+}
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+	context: req => ({
+		...req,
+		prisma: new Prisma({
+			typeDefs: typeDefs,
+			endpoint: 'http://localhost:4466',
+			debug: true,
+		}),
+	}),
+})
+server.start(() => console.log('Server is running on localhost:4000'))
